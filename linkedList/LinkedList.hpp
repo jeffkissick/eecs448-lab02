8dbed04e524ac16445ee5b911a0760481784d86d
Jeff Kissick <jkissick@ku.edu>
2019-02-14 09:54:02 -0600

    Fixed the search function

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -46,6 +46,22 @@ bool LinkedList<T>::search(T value) const
 		Fix this method
 	*/
 
+
+		while(temp != nullptr)
+		{
+
+			if(temp->getValue()==value)
+			{
+
+				isFound = true;
+
+			}
+
+			temp = temp->getNext();
+
+		}
+
+
 	return(isFound);
 
 }
